4c66aa93e1020601de8c0572b490ec874a337633
Luisfp0 <luis.oliveirabr1@gmail.com>
2024-08-08 11:45:20 -0300

    fix: error TS2353 on ChatListItem.stories, feat: add stories message visualized and messagem with audio

--- src/components/ChatListItem.stories.tsx
+++ src/components/ChatListItem.stories.tsx
@@ -12,12 +12,16 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    photo: "https://raw.githubusercontent.com/Luisfp0/desafio-estagio-frontend-aug-24/main/src/assets/hyerDev.png",
+    photo:
+      "https://raw.githubusercontent.com/Luisfp0/desafio-estagio-frontend-aug-24/main/src/assets/hyerDev.png",
     name: "Hyerdev",
-    lastMessage:
-      "Olá! Seja bem-vindo(a) à Hyerdev.",
+    message: {
+      lastMessage: "Olá! Seja bem-vindo(a) à Hyerdev.",
+      typeOfMessage: "message",
+    },
     read: true,
     lastMessageDate: "2024-08-07T14:41:13.197Z",
+    type: "DM",
   },
 };
 
@@ -25,10 +29,13 @@ export const WithoutPhoto: Story = {
   args: {
     photo: "",
     name: "Hyerdev",
-    lastMessage:
-      "Olá! Seja bem-vindo(a) à Hyerdev.",
+    message: {
+      lastMessage: "Olá! Seja bem-vindo(a) à Hyerdev.",
+      typeOfMessage: "message",
+    },
     read: true,
     lastMessageDate: "2024-08-07T14:41:13.197Z",
+    type: "DM",
   },
 };
 
@@ -36,9 +43,40 @@ export const MessageUnread: Story = {
   args: {
     photo: "https://github.com/Luisfp0.png",
     name: "Luís Fernando",
-    lastMessage:
-      "Como podemos ajudar hoje?",
+    message: {
+      lastMessage: "Como podemos ajudar hoje?",
+      typeOfMessage: "message",
+    },
     read: false,
     lastMessageDate: "2024-08-07T14:41:13.197Z",
+    type: "DM",
+  },
+};
+
+export const MessageVisualized: Story = {
+  args: {
+    photo: "https://github.com/Luisfp0.png",
+    name: "Luís Fernando",
+    message: {
+      lastMessage: "Como podemos ajudar hoje?",
+      typeOfMessage: "message",
+    },
+    read: true,
+    lastMessageDate: "2024-08-07T14:41:13.197Z",
+    type: "DM",
+  },
+};
+
+export const MessageWithAudio: Story = {
+  args: {
+    photo: "https://github.com/Luisfp0.png",
+    name: "Luís Fernando",
+    message: {
+      lastMessage: "0:43",
+      typeOfMessage: "audio",
+    },
+    read: false,
+    lastMessageDate: "2024-08-07T14:41:13.197Z",
+    type: "DM",
   },
 };
